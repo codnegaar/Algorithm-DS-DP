7a1164ff4b98f7e6d22bdb4aa4cc0f9331e10b6c
Reza <seda1390@gmail.com>
2022-07-26 19:13:04 -0700

    Update Leetcode_36_Valid Sudoku.py

--- Leetcode_36_Valid Sudoku.py
+++ Leetcode_36_Valid Sudoku.py
@@ -10,3 +10,27 @@ A Sudoku board (partially filled) could be valid but is not necessarily solvable
 Only the filled cells need to be validated according to the mentioned rules.
 
 '''
+
+
+from collections import defaultdict
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        squares = collections.defaultdict(set) # key=(r/3, c/3)
+        
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                    
+                if (board[r][c] in rows[r] or
+                    board[r][c] in cols[c] or
+                    board[r][c] in squares[r//3, c//3]):
+                    return False
+                
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r//3, c//3)].add(board[r][c])
