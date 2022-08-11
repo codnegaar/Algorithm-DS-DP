a59fbe0526609996b2aa6d2165b83f86299738af
Reza <seda1390@gmail.com>
2022-08-10 23:39:05 -0700

    Update and rename Leetcode_LinkedList_203_Remove Linked List Elements.py to Leetcode_LinkedList_237_Delete Node in a Linked List.py

--- Leetcode_LinkedList_237_Delete Node in a Linked List.py
+++ Leetcode_LinkedList_237_Delete Node in a Linked List.py
@@ -38,15 +38,6 @@ class Solution:
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
-        dummy = ListNode(next = head)
-        prev, next = dummy, head
-        
-        while curr:
-            nxt = curr.next
-            if curr.val == val:
-                prev.nex=nxt
-            else:
-                prev=curr
-            curr = curr.next
-            
-        return dummy.next
+        node.val = node.next.val
+        node.next = node.next.next
+ 
