9ccf8d666e0204046deee6cd3d068af475153d2f
Reza <seda1390@gmail.com>
2022-08-15 22:37:04 -0700

    Update Leetcode_x_Lonely Integer.py

--- Leetcode_x_Lonely Integer.py
+++ Leetcode_x_Lonely Integer.py
@@ -1,7 +1,7 @@
 '''
 Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
-Example 1:
+Example 1: 
         Input: nums = [1,2,3,1]
         Output: true
 
