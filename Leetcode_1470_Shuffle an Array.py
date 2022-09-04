044b5a10aedf314079664b8869b39d76bed7f488
Reza <seda1390@gmail.com>
2022-09-03 22:32:09 -0700

    Update and rename Leetcode_00_Shuffle an Array.py to Leetcode_1470_Shuffle an Array.py

--- Leetcode_1470_Shuffle an Array.py
+++ Leetcode_1470_Shuffle an Array.py
@@ -34,5 +34,24 @@ Constraints:
 -106 <= nums[i] <= 106
 All the elements of nums are unique.
 At most 104 calls in total will be made to reset and shuffle.
-
 '''
+
+class Solution:
+    def __init__(self, nums):
+        self.array = nums
+        self.original = list(nums)
+
+    def reset(self):
+        self.array = self.original
+        self.original = list(self.original)
+        return self.array
+
+    def shuffle(self):
+        aux = list(self.array)
+
+        for idx in range(len(self.array)):
+            remove_idx = random.randrange(len(aux))
+            self.array[idx] = aux.pop(remove_idx)
+
+        return self.array
+ 
