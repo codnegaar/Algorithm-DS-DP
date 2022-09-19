ee9155f0bc04a21202377eb7d2a257c4ddaead42
Reza <seda1390@gmail.com>
2022-09-19 10:18:04 -0700

    Create Leetcode_Tree Traverse_Top to bottom.java

---
+++ Leetcode_Tree Traverse_Top to bottom.java
@@ -0,0 +1,44 @@
+/*
+ Solve Tree Problems Recursively
+Report Issue
+"Top-down" Solution
+"Bottom-up" Solution
+Conclusion
+
+
+In previous sections, we have introduced how to solve tree traversal problems recursively. Recursion is one of the most powerful and frequently used techniques for solving tree problems.
+
+As we know, a tree can be defined recursively as a node(the root node) that includes a value and a list of references to children nodes. Recursion is one of the natural features of a tree. Therefore, many tree problems can be solved recursively. For each recursive function call, we only focus on the problem for the current node and call the function recursively to solve its children.
+
+Typically, we can solve a tree problem recursively using a top-down approach or using a bottom-up approach.
+
+"Top-down" Solution
+"Top-down" means that in each recursive call, we will visit the node first to come up with some values, and pass these values to its children when calling the function recursively. So the "top-down" solution can be considered as a kind of preorder traversal. To be specific, the recursive function top_down(root, params) works like this:
+1. return specific value for null node
+2. update the answer if needed                      // answer <-- params
+3. left_ans = top_down(root.left, left_params)      // left_params <-- root.val, params
+4. right_ans = top_down(root.right, right_params)   // right_params <-- root.val, params
+5. return the answer if needed                      // answer <-- left_ans, right_ans
+For instance, consider this problem: given a binary tree, find its maximum depth.
+
+Here we will define the depth of the root node as 1 (although often, the depth of the root node is defined as 0). For each node, if we know its depth, we will know the depth of its children. Therefore, if we pass the depth of the node as a parameter when calling the function recursively, all the nodes will know their depth. And for leaf nodes, we can use the depth to update the final answer. Here is the pseudocode for the recursive function maximum_depth(root, depth):
+
+1. return if root is null
+2. if root is a leaf node:
+3.     answer = max(answer, depth)         // update the answer if needed
+4. maximum_depth(root.left, depth + 1)     // call the function recursively for left child
+5. maximum_depth(root.right, depth + 1)    // call the function recursively for right chil 
+  
+*/
+
+private int answer; // don't forget to initialize answer before call maximum_depth
+private void maximum_depth(TreeNode root, int depth) {
+    if (root == null) {
+        return;
+    }
+    if (root.left == null && root.right == null) {
+        answer = Math.max(answer, depth);
+    }
+    maximum_depth(root.left, depth + 1);
+    maximum_depth(root.right, depth + 1);
+}
