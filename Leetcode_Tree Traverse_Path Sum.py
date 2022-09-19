8afebb4c3540701c63649d5456e436950d0da871
Reza <seda1390@gmail.com>
2022-09-19 11:26:22 -0700

    Create Leetcode_Tree Traverse_Path Sum.py

---
+++ Leetcode_Tree Traverse_Path Sum.py
@@ -0,0 +1,48 @@
+'''
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+A leaf is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true
+Explanation: The root-to-leaf path with the target sum is shown.
+Example 2:
+
+
+Input: root = [1,2,3], targetSum = 5
+Output: false
+Explanation: There two root-to-leaf paths in the tree:
+(1 --> 2): The sum is 3.
+(1 --> 3): The sum is 4.
+There is no root-to-leaf path with sum = 5.
+
+'''
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+           
+        def dfs(node, currSum):
+            if not node:
+                return False
+            
+            # If it is a leaf node
+            currSum += node.val        
+            if not node.left and not node.right:
+                return currSum == targetSum
+            
+            # if not the leaf node
+            return (dfs(node.left, currSum) or dfs(node.right, currSum))
+                
+        return dfs(root, 0)
+        
