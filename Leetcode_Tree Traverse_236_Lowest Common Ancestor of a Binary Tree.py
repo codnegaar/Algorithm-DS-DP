6349fbcebdfa4d3318353fe82d78d2a1ccc0651e
Reza <seda1390@gmail.com>
2022-09-22 15:02:57 -0700

    Update Leetcode_Tree Traverse_236_Lowest Common Ancestor of a Binary Tree.py

--- Leetcode_Tree Traverse_236_Lowest Common Ancestor of a Binary Tree.py
+++ Leetcode_Tree Traverse_236_Lowest Common Ancestor of a Binary Tree.py
@@ -30,7 +30,6 @@ Constraints:
           p and q will exist in the tree.
 
 '''
-
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, x):
@@ -40,15 +39,15 @@ class TreeNode:
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
+        if root == p or root == q:
+            return root
+        l = self.lowestCommonAncestor(root.left,p,q)
+        r = self.lowestCommonAncestor(root.right,p,q)
         
-        cur = root
-        while cur:
-            if p.val > cur.val and q.val > cur.val:
-                cur = cur.right
-            elif p.val < cur.val and q.val < cur.val:
-                cur = cur.left
-            else:
-                return cur
+        if l and r:
+            return root
+        else:
+            return l or r
         
-
-
