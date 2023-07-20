7e576d74a14225cb5e6e8269c90722bfb867eac1
Reza <seda1390@gmail.com>
2023-07-20 10:25:57 -0700

    Create Leetcode  516 Longest Palindromic Subsequence.py

---
+++ Leetcode  516 Longest Palindromic Subsequence.py
@@ -0,0 +1,32 @@
+'''
+Given a string s, find the longest palindromic subsequence's length in s.
+A subsequence is a sequence that can be derived from another sequence by deleting some or no elements without changing the order of the remaining elements.
+
+Example 1:
+        Input: s = "bbbab"
+        Output: 4
+        Explanation: One possible longest palindromic subsequence is "bbbb".
+
+Example 2:
+        Input: s = "cbbd"
+        Output: 2
+        Explanation: One possible longest palindromic subsequence is "bb".
+ 
+Constraints:
+        1 <= s.length <= 1000
+        s consists only of lowercase English letters
+'''
+class Solution:
+  def longestPalindromeSubseq(self, s: str) -> int:
+    # dp(i, j) := LPS's length in s[i..j]
+    @functools.lru_cache(None)
+    def dp(i: int, j: int) -> int:
+      if i > j:
+        return 0
+      if i == j:
+        return 1
+      if s[i] == s[j]:
+        return 2 + dp(i + 1, j - 1)
+      return max(dp(i + 1, j), dp(i, j - 1))
+
+    return dp(0, len(s) - 1)
