d3499a5936b70ef838777bf952eea29092271703
Reza <seda1390@gmail.com>
2023-08-02 08:33:22 -0700

    Create Leetcode 354 Russian Doll Envelopes.py

---
+++ Leetcode 354 Russian Doll Envelopes.py
@@ -0,0 +1,44 @@
+'''
+
+354 Russian Doll Envelopes
+You are given a 2D array of integer envelopes where envelopes[i] = [wi, hi] represent an envelope's width and height.
+One envelope can fit into another if and only if both the width and height of one envelope are greater than the other envelope's width and height.
+Return the maximum number of envelopes you can Russian doll (i.e., put one inside the other).
+Note: You cannot rotate an envelope.
+ 
+Example 1:
+        Input: envelopes = [[5,4],[6,4],[6,7],[2,3]]
+        Output: 3
+        Explanation: The maximum number of envelopes you can make a Russian doll is 3 ([2,3] => [5,4] => [6,7]).
+
+Example 2:
+        Input: envelopes = [[1,1],[1,1],[1,1]]
+        Output: 1 
+
+Constraints:
+        1 <= envelopes.length <= 105
+        envelopes[i].length == 2
+        1 <= wi, hi <= 105
+'''
+class Solution:
+  def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+    envelopes.sort(key=lambda x: (x[0], -x[1]))
+    # Same as 300. Longest Increasing Subsequence
+    ans = 0
+    dp = [0] * len(envelopes)
+
+    for _, h in envelopes:
+      l = 0
+      r = ans
+      while l < r:
+        m = (l + r) // 2
+        if dp[m] >= h:
+          r = m
+        else:
+          l = m + 1
+      dp[l] = h
+      if l == ans:
+        ans += 1
+
+    return ans
+
