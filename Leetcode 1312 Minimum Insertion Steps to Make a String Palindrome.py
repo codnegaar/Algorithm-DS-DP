f53be40969fa324165157ab25e3e3ecfd60e38c0
Reza <seda1390@gmail.com>
2023-08-13 06:50:26 -0700

    Create Leetcode 1312 Minimum Insertion Steps to Make a String Palindrome.py

---
+++ Leetcode 1312 Minimum Insertion Steps to Make a String Palindrome.py
@@ -0,0 +1,36 @@
+'''
+1312 Minimum Insertion Steps to Make a String Palindrome 
+
+Given a string s. In one step you can insert any character at any index of the string.
+Return the minimum number of steps to make s palindrome.
+A Palindrome String is one that reads the same backward as well as forward.
+
+Example 1:
+        Input: s = "zzazz"
+        Output: 0
+        Explanation: The string "zzazz" is already palindrome we do not need any insertions.
+
+Example 2:
+        Input: s = "mbadm"
+        Output: 2
+        Explanation: String can be "mbdadbm" or "mdbabdm".
+
+Example 3:
+        Input: s = "leetcode"
+        Output: 5
+        Explanation: Inserting 5 characters the string becomes "leetcodocteel". 
+
+Constraints:
+        1 <= s.length <= 500
+        s consists of lowercase English letters.
+
+'''
+
+class Solution:
+    def minInsertions(self, s: str) -> int:
+        @lru_cache(None)
+        def rec(i,j):
+            if(i>=j ) : return 0
+            elif(s[i]==s[j]): return rec(i+1,j-1) 
+            else: return 1+ min( rec(i+1,j) , rec(i,j-1))
+        return rec(0,len(s)-1)
