fce34635df18f4d8e6f1d18377e63566ee8baece
Reza <seda1390@gmail.com>
2023-09-18 10:47:54 -0700

    Create Leetcode 382 Linked List Random Node.py

---
+++ Leetcode 382 Linked List Random Node.py
@@ -0,0 +1,96 @@
+'''
+382 Linked List Random Node
+
+Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+Implement the Solution class:
+Solution(ListNode head) Initializes the object with the head of the singly-linked list head.
+int getRandom() Chooses a node randomly from the list and returns its value. All the nodes of the list should be equally likely to be chosen.
+ 
+
+Example 1:
+        Input
+        ["Solution", "getRandom", "getRandom", "getRandom", "getRandom", "getRandom"]
+        [[[1, 2, 3]], [], [], [], [], []]
+        Output
+        [null, 1, 3, 2, 2, 3]
+        
+        Explanation
+        Solution solution = new Solution([1, 2, 3]);
+        solution.getRandom(); // return 1
+        solution.getRandom(); // return 3
+        solution.getRandom(); // return 2
+        solution.getRandom(); // return 2
+        solution.getRandom(); // return 3
+        // getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
+ 
+
+Constraints:
+        The number of nodes in the linked list will be in the range [1, 104].
+        -104 <= Node.val <= 104
+        At most 104 calls will be made to getRandom.
+ 
+
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def __init__(self, head: Optional[ListNode]):
+        self.head = head
+
+    def getRandom(self) -> int:
+        n = ans = 0
+        head = self.head
+        while head:
+            n += 1
+            x = random.randint(1, n)
+            if n == x:
+                ans = head.val
+            head = head.next
+        return ans
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(head)
+# param_1 = obj.getRandom()
+
+############
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+import random
+
+
+class Solution(object):
+
+  def __init__(self, head):
+    """
+    @param head The linked list's head.
+    Note that the head is guaranteed to be not null, so it contains at least one node.
+    :type head: ListNode
+    """
+    self.head = head
+
+  def getRandom(self):
+    """
+    Returns a random node's value.
+    :rtype: int
+    """
+    ans = self.head.val
+    head = self.head
+    idx = 1
+    while head:
+      if random.randrange(1, idx + 1) == idx:
+        ans = head.val
+      head = head.next
+      idx += 1
+    return ans
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(head)
+# param_1 = obj.getRandom()
