73b1c70250704085ef509432df7d6eb3a8c4ac2a
Reza <seda1390@gmail.com>
2023-12-05 07:39:03 -0800

    Update Leetcode DFS 341 Flatten Nested List Iterator.py

--- Leetcode DFS 341 Flatten Nested List Iterator.py
+++ Leetcode DFS 341 Flatten Nested List Iterator.py
@@ -4,10 +4,10 @@ Leetcode DFS 341 Flatten Nested List Iterator
 You are given a nested list of integers nestedList. Each element is either an integer or a list whose elements may also be integers or other lists. Implement an iterator to flatten it.
 
 Implement the NestedIterator class:
-
-NestedIterator(List<NestedInteger> nestedList) Initializes the iterator with the nested list nestedList.
-int next() Returns the next integer in the nested list.
-boolean hasNext() Returns true if there are still some integers in the nested list and false otherwise.
+                NestedIterator(List<NestedInteger> nestedList) Initializes the iterator with the nested list nestedList.
+                int next() Returns the next integer in the nested list.
+                boolean hasNext() Returns true if there are still some integers in the nested list and false otherwise.
+                
 Your code will be tested with the following pseudocode:
 
 initialize iterator with nestedList
