f82fa187b92891a37f422d31e81fd35acf8ae2ea
Reza <seda1390@gmail.com>
2024-01-30 08:55:58 -0800

    Update and rename Leetcode_283_ Array_Move Zeroes.py to Leetcode 75-Two Pointers 283 Array Move Zeroes.py

--- Leetcode 75-Two Pointers 283 Array Move Zeroes.py
+++ Leetcode 75-Two Pointers 283 Array Move Zeroes.py
@@ -1,15 +1,12 @@
 '''
 
+Leetcode 75-Two Pointers 283 Array Move Zeroes
+
 Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 Note that you must do this in-place without making a copy of the array.
 
 
 '''
-
-"""
-Do not return anything, modify nums in-place instead.
-"""
-
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         lptr = 0
@@ -17,3 +14,12 @@ class Solution:
             if nums[rptr]:
                 nums[lptr], nums [rptr] = nums [rptr],  nums[lptr]
                 lptr += 1
+
+# second solution
+﻿class Solution:
+    def moveZeroes (self, nums: List[int]) -> None:
+        base=0
+        for i in range(len(nums)):
+            if nums[i]=0:
+                nums[base], nums[i] = nums[i], nums[base] 
+                base += 1
