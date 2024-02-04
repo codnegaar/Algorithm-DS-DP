069eb76c8958f8d1b3680af4abb223fbe17a125e
Reza <seda1390@gmail.com>
2024-02-04 08:28:57 -0800

    Update and rename Leetcode_104_Maximum Depth of Binary Tree.py to Leetcode 75-Binary Tree - DFS 104 Maximum Depth of Binary Tree.py

--- Leetcode 75-Binary Tree - DFS 104 Maximum Depth of Binary Tree.py
+++ Leetcode 75-Binary Tree - DFS 104 Maximum Depth of Binary Tree.py
@@ -1,4 +1,6 @@
 '''
+Leetcode 75-Binary Tree - DFS 104 Maximum Depth of Binary Tree
+
 Given the root of a binary tree, return its maximum depth. A binary tree's maximum depth is the number of nodes along 
 the longest path from the root node down to the farthest leaf node. 
 
@@ -6,14 +8,13 @@ Example 1:
         Input: root = [3,9,20,null,null,15,7]
         Output: 3
 Example 2:
-        Input: root = [1,null,2]
+        Input: root = [1, null, 2]
         Output: 2
 Constraints:
         The number of nodes in the tree is in the range [0, 104].
         -100 <= Node.val <= 100
 
 '''
-
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -27,3 +28,19 @@ class Solution:
             return 0
         
         return 1+ max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+# Second Solution
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def dfs(root, depth):
+            if not root: return depth
+            return max(dfs(root.left, depth + 1), dfs(root.right, depth + 1))                       
+        return dfs(root, 0)
+        
