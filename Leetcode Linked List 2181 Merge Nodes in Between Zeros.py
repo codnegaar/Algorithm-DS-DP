45aad466df0a410b3c46c0107df0420617d11e82
Reza <seda1390@gmail.com>
2024-07-04 11:08:41 -0700

    Update Leetcode Linked List 2181 Merge Nodes in Between Zeros.py

--- Leetcode Linked List 2181 Merge Nodes in Between Zeros.py
+++ Leetcode Linked List 2181 Merge Nodes in Between Zeros.py
@@ -54,4 +54,37 @@ class Solution:
     next.val += head.val
     return next
 
+
+# scond solution
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+
+class Solution:
+    def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # BASE CASE -> if we have a single zero, simply return null
+        if not head.next:
+            return None
+        
+        # fetch sum from current 0 to next 0
+        ptr = head.next
+        sum = 0
+        while ptr.val != 0:
+            sum += ptr.val
+            ptr = ptr.next
+        
+        # assign sum on the first node between nodes having value 0
+        head.next.val = sum
+        
+        # call and get the answer and connect the answer to next of head->next
+        head.next.next = self.mergeNodes(ptr)
+        
+        # return head->next..=> new head
+        return head.next
+        
+
         
