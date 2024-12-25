a37fa92aee5879428162e829afbc6cf128422df4
Reza <seda1390@gmail.com>
2024-12-24 20:02:16 -0800

    Update Leetcode DFS 515 Find Largest Value in Each Tree Row.py

--- Leetcode DFS 515 Find Largest Value in Each Tree Row.py
+++ Leetcode DFS 515 Find Largest Value in Each Tree Row.py
@@ -1,7 +1,8 @@
 '''
-Leetcode DFS 515 Find the Largest Value in Each Tree Row
 
-Given the root of a binary tree, return an array of the largest value in each row of the tree (0-indexed).
+Leetcode BFS 515 Find the Largest Value in Each Tree Row
+
+Given the root of a binary tree, return an array of the largest value in each tree row (0-indexed).
 
 Example 1:
         Input: root = [1,3,2,5,3,null,9]
@@ -42,4 +43,40 @@ class Solution:
     dfs(root, 0)
     return ans
 
+# Second solution
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+
+        res = []
+        q = deque([root])
+
+        while q:
+            max_level = q[0].val
+            len_q = len(q)
+
+            for _ in range(len_q):
+                node = q.popleft()
+                max_level =max(max_level, node.val)
+
+                if node.left: 
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            res.append(max_level)
+        return res
+
+
+
+
+        
 
