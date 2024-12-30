f3d9712810c4a522ae899ba4b04dba063e6e9dc6
Reza <seda1390@gmail.com>
2024-12-30 07:52:45 -0800

    Update Leetcode 2466 Count Ways To Build Good Strings.py

--- Leetcode 2466 Count Ways To Build Good Strings.py
+++ Leetcode 2466 Count Ways To Build Good Strings.py
@@ -1,4 +1,5 @@
 '''
+
 2466 Count Ways To Build Good Strings
  
 Given the integers zero, one, low, and high, we can construct a string by starting with an empty string, and then at each step perform either of the following:
