9f5d7f66e5a4ad6c286cab8da44b4aa15dbefb6c
Reza <seda1390@gmail.com>
2025-04-05 17:04:53 -0700

    Update and rename Lettcode_242_String_Valid Anagram.py to Lettcode 242 String Valid Anagram.py

--- Lettcode 242 String Valid Anagram.py
+++ Lettcode 242 String Valid Anagram.py
@@ -1,10 +1,8 @@
 '''
+Lettcode 242 String Valid Anagram.py
 
 Given two strings s and t, return true if t is an anagram of s, and false otherwise.
-
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
-
- 
 
 Example 1:
         Input: s = "anagram", t = "nagaram"
@@ -43,17 +41,9 @@ class Solution:
             
 
  # Third solution
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return sorted(s) == sorted(t)
-   
-   
-
-        
-        
-        
-        
         
         
               
